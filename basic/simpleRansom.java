04ab0f462cc9c07e4f6d6561137b0bf08287241d
Christian Pari <christianpari@outlook.com>
2020-12-01 20:29:59 -0500

    Simple algorithm using HashMaps to return true or false if all letters within two Strings match

---
+++ basic/simpleRansom.java
@@ -0,0 +1,37 @@
+import java.util.HashMap;
+
+public class simpleRansom {
+  public static void main(String[] args) {
+    System.out.println(simpleRansom("ac", "abc"));
+  }
+
+  public static boolean simpleRansom(String message, String source) {
+    HashMap<String, Integer> letterCount = new HashMap<>();
+    String[] sourceArray = source.split("");
+
+    for (var letter : sourceArray) {
+      if (letter.equals(" ")) {
+        continue;
+      }
+
+      if (letterCount.containsKey(letter)) {
+        letterCount.replace(letter, letterCount.get(letter) + 1);
+        continue;
+      }
+
+      letterCount.put(letter, 1);
+    }
+
+    String[] messageArray = message.split("");
+    for (var letter : messageArray) {
+      if (!letterCount.containsKey(letter)) return false;
+
+      if (letterCount.get(letter) == 0) return false;
+
+      letterCount.replace(letter, letterCount.get(letter) - 1);
+    }
+
+    return true;
+  }
+
+}
